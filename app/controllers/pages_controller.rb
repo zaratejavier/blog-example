976508d40d54a623b4a5cdec536d8e613fd2e80a
Javier Zarate <zarate.javier.jz@gmail.com>
2020-05-18 14:55:51 -0600

    got everything to work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,8 +8,52 @@ class PagesController < ApplicationController
   end
 
   def show
+    #find a page but we need an id
+    @page = Page.find(params[:id])
   end
 
   def new
+    @page = Page.new
+    #go to a folder views/pages/new.hmtl/etc
   end
+
+  
+
+  def create
+   @page = Page.new(pages_params)
+   if @pages.save
+    redirect_to pages_path
+   else
+    render :new
+   end
+  end
+
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+
+    if (@page.update(pages_params))
+      redirect_to pages_path
+      else
+        render :edit
+      end
+  end
+
+  def destroy
+    @page = Page.find(params[:id])
+    @page.destroy
+    redirect_to pages_path
+
+  end
+
+
+  private
+    
+  def pages_params
+    params.require(:page).permit(:title, :author, :body)
+  end
+
 end
